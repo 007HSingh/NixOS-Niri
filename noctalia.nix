62e97bf152dc44df3ed952ddf66d68fe9fcfbd2a
Harsh Singh <singhharsh25032008@gmail.com>
2026-01-01 17:16:45 +0530

    Added yazi, changed theme to catppuccin and added catwalk plugin

--- noctalia.nix
+++ noctalia.nix
@@ -25,9 +25,9 @@
 
     home.file.".cache/noctalia/wallpapers.json" = {
       text = builtins.toJSON {
-        defaultWallpaper = "/home/harsh/Pictures/Wallpapers/wallpaper(9).png";
+        defaultWallpaper = "/home/harsh/Pictures/Wallpapers/wallpaper(9).jpg";
         wallpapers = {
-          "eDP-1" = "/home/harsh/Pictures/Wallpapers/wallpaper(9).png";
+          "eDP-1" = "/home/harsh/Pictures/Wallpapers/wallpaper(9).jpg";
         };
       };
     };
