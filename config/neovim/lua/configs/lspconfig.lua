1e3ecc4dbeba79ab00608c443024e502667ce0f4
Harsh Singh <singhharsh25032008@gmail.com>
2026-01-05 17:41:39 +0530

    update: neovim LSPs deprectated changes removed

--- config/neovim/lua/configs/lspconfig.lua
+++ config/neovim/lua/configs/lspconfig.lua
@@ -142,12 +142,6 @@ vim.diagnostic.config {
   },
 }
 
-local signs = { Error = " ", Warn = " ", Hint = "󰌵 ", Info = " " }
-for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-end
-
 vim.diagnostic.config {
   virtual_text = {
     prefix = "●",
