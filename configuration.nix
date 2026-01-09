b720ce1afc37f7d07b13e2c230eb512b6210effc
Harsh Singh <singhharsh25032008@gmail.com>
2026-01-09 17:47:24 +0530

    update: flakes updated

--- configuration.nix
+++ configuration.nix
@@ -182,7 +182,7 @@
 
   programs.thunar = {
     enable = true;
-    plugins = with pkgs; [
+    plugins = with pkgs.xfce; [
       thunar-volman
       thunar-archive-plugin
       thunar-vcs-plugin
