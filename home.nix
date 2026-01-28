d041d8d28a2257c394c5e879ff5d7a1a4ab40222
Harsh Singh <singhharsh25032008@gmail.com>
2026-01-28 07:20:27 +0530

    update(home): gtk theming

--- home.nix
+++ home.nix
@@ -59,6 +59,14 @@ in
       name = "Papirus-Dark";
       package = pkgs.papirus-icon-theme;
     };
+
+    gtk3.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
+
+    gtk4.extraConfig = {
+      gtk-application-prefer-dark-theme = 1;
+    };
   };
 
   xdg = {
