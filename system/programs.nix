d79e482e5ef042748f4496f8273d08357ddba522
Harsh Singh <singhharsh25032008@gmail.com>
2026-02-03 07:28:08 +0530

    feat: enhance VSCode configuration with additional extensions and marketplace support

--- system/programs.nix
+++ system/programs.nix
@@ -7,23 +7,38 @@
 
   programs.vscode = {
     enable = true;
-    extensions = with pkgs.vscode-extensions; [
-      catppuccin.catppuccin-vsc
-      catppuccin.catppuccin-vsc-icons
-      jnoortheen.nix-ide
-      github.copilot-chat
-      github.copilot
-      ms-python.vscode-pylance
-      ms-python.python
-      ms-python.debugpy
-      ms-python.flake8
-      ms-vscode.cpptools-extension-pack
-      redhat.java
-      vscjava.vscode-java-pack
-      vscjava.vscode-spring-initializr
-      ms-vscode-remote.remote-containers
-      docker.docker
-      eamodio.gitlens
+    extensions = with pkgs; [
+      (vscode-with-extensions.override {
+        vscode-extensions =
+          with vscode-extensions;
+          [
+            catppuccin.catppuccin-vsc
+            catppuccin.catppuccin-vsc-icons
+            jnoortheen.nix-ide
+            github.copilot-chat
+            github.copilot
+            ms-python.vscode-pylance
+            ms-python.python
+            ms-python.debugpy
+            ms-python.flake8
+            ms-vscode.cpptools-extension-pack
+            redhat.java
+            vscjava.vscode-java-pack
+            vscjava.vscode-spring-initializr
+            ms-vscode-remote.remote-containers
+            docker.docker
+            ms-azuretools.vscode-docker
+            eamodio.gitlens
+          ]
+          ++ pkgs.vscode-extensionsFromVscodeMarketplace [
+            {
+              name = "vscode-containers";
+              publisher = "ms-azuretools";
+              version = "2.4.0";
+              sha256 = "0q6c65qh96nl7qdmk58hm2sipcsv3xj9vvfvrvhl4w36k9zbar4q";
+            }
+          ];
+      })
     ];
   };
 
