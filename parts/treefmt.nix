a2126560521ddec5ca8bf91b33b93f063b1fb703
Harsh Singh <singhharsh25032008@gmail.com>
2026-04-06 17:47:42 +0530

    fix: format parts/treefmt.nix with nixfmt

--- parts/treefmt.nix
+++ parts/treefmt.nix
@@ -2,34 +2,32 @@
 _:
 
 {
-  perSystem =
-    _:
-    {
-      # Treefmt configuration
-      treefmt = {
-        # Used to find the project root
-        projectRootFile = "flake.nix";
+  perSystem = _: {
+    # Treefmt configuration
+    treefmt = {
+      # Used to find the project root
+      projectRootFile = "flake.nix";
 
-        # Individual formatters
-        programs = {
-          # Nix
-          nixfmt.enable = true;
+      # Individual formatters
+      programs = {
+        # Nix
+        nixfmt.enable = true;
 
-          # Lua
-          stylua.enable = true;
+        # Lua
+        stylua.enable = true;
 
-          # Shell
-          shfmt.enable = true;
+        # Shell
+        shfmt.enable = true;
 
-          # JSON, YAML, Markdown, etc.
-          prettier.enable = true;
-        };
+        # JSON, YAML, Markdown, etc.
+        prettier.enable = true;
+      };
 
-        # Settings for specific formatters
-        settings.formatter = {
-          shfmt.includes = [ "*.sh" ];
-          stylua.includes = [ "*.lua" ];
-        };
+      # Settings for specific formatters
+      settings.formatter = {
+        shfmt.includes = [ "*.sh" ];
+        stylua.includes = [ "*.lua" ];
       };
     };
+  };
 }
