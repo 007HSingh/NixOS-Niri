dc2de1fb29d5f041f0ce91a5fdfafde15da3be2f
Harsh Singh <singhharsh25032008@gmail.com>
2026-04-06 17:54:18 +0530

    chore: redefine Catppuccin Mocha color scheme inline in stylix.nix

--- system/stylix.nix
+++ system/stylix.nix
@@ -7,9 +7,25 @@
     enable = true;
     autoEnable = true;
 
-    # Color scheme: Catppuccin Mocha
-    # Can also be set to path from base16-schemes if desired
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    # Color scheme: Catppuccin Mocha (defined inline)
+    base16Scheme = {
+      base00 = "1e1e2e"; # bg
+      base01 = "313244"; # surface0
+      base02 = "45475a"; # surface1
+      base03 = "585b70"; # surface2
+      base04 = "6c7086"; # overlay0
+      base05 = "cdd6f4"; # text
+      base06 = "f5f5f5"; # rosewater
+      base07 = "ffffff"; # white
+      base08 = "f38ba8"; # red
+      base09 = "fab387"; # peach
+      base0A = "f9e2af"; # yellow
+      base0B = "a6e3a1"; # green
+      base0C = "94e2d5"; # teal
+      base0D = "89b4fa"; # blue
+      base0E = "cba6f7"; # mauve
+      base0F = "f2cdcd"; # flamingo
+    };
 
     # Polarity must be set to either "dark" or "light"
     polarity = "dark";
