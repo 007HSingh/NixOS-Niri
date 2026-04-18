b03532a9e2767ac57d4a7e51465577e93051cb44
Harsh Singh <singhharsh25032008@gmail.com>
2026-04-18 09:29:59 +0530

    refractor: rewrote termipedia.nix for flake check

--- home/termipedia.nix
+++ home/termipedia.nix
@@ -4,22 +4,34 @@
 { pkgs, ... }:
 
 let
-  termipedia = pkgs.writeShellApplication {
-    name = "termipedia";
+  src = pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/kantiankant/Termipedia/main/termipedia.sh";
+    hash = "sha256-mnmQZ5R9sEwL6J55PTeP3jaOP6MbqQ711FEoOH7BRkY=";
+  };
 
-    runtimeInputs = with pkgs; [
-      curl
-      jq
-      fzf
-      less
-    ];
+  termipedia = pkgs.stdenv.mkDerivation {
+    pname = "termipedia";
+    version = "unstable";
 
-    text = builtins.readFile (
-      pkgs.fetchurl {
-        url = "https://raw.githubusercontent.com/kantiankant/Termipedia/main/termipedia.sh";
-        hash = "sha256-mnmQZ5R9sEwL6J55PTeP3jaOP6MbqQ711FEoOH7BRkY=";
-      }
-    );
+    dontUnpack = true;
+
+    nativeBuildInputs = [ pkgs.makeWrapper ];
+
+    installPhase = ''
+      install -Dm755 ${src} $out/bin/termipedia
+      wrapProgram $out/bin/termipedia \
+        --prefix PATH : ${
+          pkgs.lib.makeBinPath (
+            with pkgs;
+            [
+              curl
+              jq
+              fzf
+              less
+            ]
+          )
+        }
+    '';
   };
 in
 {
