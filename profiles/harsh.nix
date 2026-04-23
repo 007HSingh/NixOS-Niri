f30656fd669a7046c4c5094406c16ed10248ffba
Harsh Singh <singhharsh25032008@gmail.com>
2026-04-23 18:22:36 +0530

    feat: add user and host profiles

---
+++ profiles/harsh.nix
@@ -0,0 +1,26 @@
+# Profile: harsh
+# Enables all home modules for the harsh user.
+# Imported by users/harsh/default.nix via inputs.self.homeManagerModules.profile-harsh.
+{ ... }:
+
+{
+  modules.home = {
+    audio.enable = true;
+    bar.enable = true;
+    browser.enable = true;
+    clipboard.enable = true;
+    editor.enable = true;
+    git.enable = true;
+    idle.enable = true;
+    media.enable = true;
+    notes.enable = true;
+    packages.enable = true;
+    quickshell.enable = true;
+    shell.enable = true;
+    termipedia.enable = true;
+    theming.enable = true;
+    vscode.enable = true;
+    wofi.enable = true;
+    xdg.enable = true;
+  };
+}
