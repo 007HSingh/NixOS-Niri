3dc763aeb3ad27ab43fd36e6ee4cec5db5403747
Harsh Singh <singhharsh25032008@gmail.com>
2026-04-23 22:10:26 +0530

    refactor: revert quickshell override and use default package instead

--- modules/home/quickshell/default.nix
+++ modules/home/quickshell/default.nix
@@ -1,6 +1,5 @@
 # Quickshell Configuration
 # Wallpaper picker and other shell widgets
-# Override quickshell to include QtMultimedia for video wallpaper preview support
 {
   lib,
   config,
@@ -11,19 +10,14 @@
 
 let
   cfg = config.modules.home.quickshell;
-
-  quickshellWithQtMultimedia =
-    inputs.quickshell.packages.${pkgs.stdenv.hostPlatform.system}.quickshell.overrideAttrs
-      (old: {
-        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ pkgs.qt6.qtmultimedia ];
-        buildInputs = (old.buildInputs or [ ]) ++ [ pkgs.qt6.qtmultimedia ];
-      });
 in
 {
   options.modules.home.quickshell.enable =
-    lib.mkEnableOption "quickshell (wallpaper picker, QtMultimedia)";
+    lib.mkEnableOption "quickshell (wallpaper picker)";
 
   config = lib.mkIf cfg.enable {
-    home.packages = [ quickshellWithQtMultimedia ];
+    home.packages = [
+      inputs.quickshell.packages.${pkgs.stdenv.hostPlatform.system}.default
+    ];
   };
 }
