1691d795d753d15835870fb411ec8b9384c601a3
Harsh Singh <singhharsh25032008@gmail.com>
2026-04-27 20:57:24 +0530

    refactor: remove explicit border and shadow color configurations from Hyprland settings

--- modules/home/hyprland/default.nix
+++ modules/home/hyprland/default.nix
@@ -41,8 +41,6 @@ in
           gaps_in = 5;
           gaps_out = 10;
           border_size = 2;
-          "col.active_border" = "rgba(33ccffee) rgba(00ff99ee) 45deg";
-          "col.inactive_border" = "rgba(595959aa)";
           layout = "dwindle";
         };
 
@@ -56,7 +54,6 @@ in
           drop_shadow = true;
           shadow_range = 4;
           shadow_render_power = 3;
-          "col.shadow" = "rgba(1a1a1aee)";
         };
 
         animations = {
