868d4749e62302bda809a8c8b0a447852ed9b4de
Harsh Singh <singhharsh25032008@gmail.com>
2026-04-27 21:01:10 +0530

    fix: correct workspace mapping for keybind 3 in Hyprland config

--- modules/home/hyprland/default.nix
+++ modules/home/hyprland/default.nix
@@ -107,7 +107,7 @@ in
           # Switch workspaces with mainMod + [0-9]
           "$mainMod, 1, workspace, 1"
           "$mainMod, 2, workspace, 2"
-          "$mainMod, 3, workspace, 4"
+          "$mainMod, 3, workspace, 3"
           "$mainMod, 4, workspace, 4"
           "$mainMod, 5, workspace, 5"
           "$mainMod, 6, workspace, 6"
