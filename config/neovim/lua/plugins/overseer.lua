b3285fa9a2a5d59ae2495cfe44754a61bfd6cdab
Harsh Singh <singhharsh25032008@gmail.com>
2026-05-27 01:45:03 +0530

    chore: update overseer toggle keybinding to <leader>Ot

--- config/neovim/lua/plugins/overseer.lua
+++ config/neovim/lua/plugins/overseer.lua
@@ -2,7 +2,7 @@ return {
 	"stevearc/overseer.nvim",
 	cmd = { "OverseerRun", "OverseerToggle", "OverseerBuild" },
 	keys = {
-		{ "<leader>ot", "<cmd>OverseerToggle<cr>", desc = "Overseer: toggle panel" },
+		{ "<leader>Ot", "<cmd>OverseerToggle<cr>", desc = "Overseer: toggle panel" },
 		{ "<leader>or", "<cmd>OverseerRun<cr>", desc = "Overseer: run task" },
 		{ "<leader>ob", "<cmd>OverseerBuild<cr>", desc = "Overseer: build" },
 		{ "<leader>oa", "<cmd>OverseerTaskAction<cr>", desc = "Overseer: task action" },
