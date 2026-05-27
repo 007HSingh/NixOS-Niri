d341871b0f25d816c5cc52561936de376b948705
Harsh Singh <singhharsh25032008@gmail.com>
2026-05-27 19:05:35 +0530

    working on potential fixes to vesktop

--- modules/home/discord/default.nix
+++ modules/home/discord/default.nix
@@ -14,6 +14,35 @@ in
   config = lib.mkIf cfg.enable {
     programs.vesktop = {
       enable = true;
+      settings = {
+        splashTheming = true;
+        checkUpdates = false;
+        customTitleBar = false;
+        hardwareAcceleration = true;
+        discordBranch = "stable";
+        minimizeToTray = false;
+      };
+      vencord = {
+        useSystem = false;
+        settings = {
+          notifyAboutUpdates = false;
+          autoUpdate = false;
+          autoUpdateNotification = false;
+          useQuickCss = true;
+          plugins = {
+            NoTrack.enabled = true;
+            ClearURLs.enabled = true;
+            MessageLinkEmbeds.enabled = true;
+            GifPaste.enabled = true;
+            PermissionsViewer.enabled = true;
+            ShowHiddenChannels.enabled = true;
+            BetterRoleContext.enabled = true;
+            FakeNitro.enabled = true;
+            ClientTheme.enabled = false;
+          };
+        };
+      };
+
     };
 
     catppuccin.vesktop = {
