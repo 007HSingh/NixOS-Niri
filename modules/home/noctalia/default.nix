e09717b7d69bfdf99947a29a2d0d80bfaf616cfe
Harsh Singh <singhharsh25032008@gmail.com>
2026-06-09 02:16:49 +0530

    Noctalia UI polished

--- modules/home/noctalia/default.nix
+++ modules/home/noctalia/default.nix
@@ -21,7 +21,7 @@ in
       enable = true;
       settings = ''
         [shell]
-        avatar_path = "~/.face"
+        font_family = "Inter"
 
         [shell.panel]
         transparency_mode = "glass"
@@ -29,9 +29,10 @@ in
         [wallpaper]
         enabled = true
         fill_mode = "crop"
-        directory = "~/Pictures/Wallpapers"
+        directory = "~/.config/wallpapers"
         transition = ["fade", "wipe", "disc", "stripes", "zoom", "honeycomb"]
         transition_duration = 1500
+        transition_on_startup = true
 
         [wallpaper.default]
         path = "~/.config/wallpapers/wallpaper-38.png"
@@ -40,15 +41,54 @@ in
         enabled = false
 
         [backdrop]
-        enabled = false
+        enabled = true
 
         [theme]
         mode = "dark"
         source = "builtin"
         builtin = "Catppuccin"
 
+        [bar]
+        order = ["main"]
+
         [bar.main]
-        position = "left"
+        position = "top"
+        thickness = 25
+        background_opacity = 0.60
+        margin_edge = 5
+        margin_ends = 20
+        radius = 12
+        padding = 10
+        widget_spacing = 10
+        scale = 0.85
+        capsule = false
+        shadow = true
+        reserve_space = true
+        auto_hide = false
+        start = ["control-center", "wallpaper", "network", "bluetooth", "bongo-cat", "launcher"]
+        center = ["audio-visualizer", "workspaces", "clock", "media", "temperature"]
+        end = ["clipboard", "caffeine","volume", "brightness", "battery", "date", "weather", "session"]
+
+        [nightlight]
+        enabled = true
+        temperature_day = 6500
+        temperature_night = 3000
+
+        [location]
+        address = "Kolkata, India"
+
+        [dock]
+        enabled = false
+
+        [notification]
+        layer = "overlay"
+        background_opacity = 0.85
+        offset_x = 15
+        offset_y = 8
+
+        [weather]
+        enabled = true
+        unit = "celsius"
       '';
     };
   };
