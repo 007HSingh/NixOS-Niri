e62530b29279c3dd2d4da060956f8540958d19d3
Harsh Singh <singhharsh25032008@gmail.com>
2026-06-11 13:52:58 +0530

    feat: added nvim-treesitter-context

--- config/neovim/lua/plugins/treesitter.lua
+++ config/neovim/lua/plugins/treesitter.lua
@@ -1,38 +1,52 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	branch = "main",
-	build = ":TSUpdate",
-	lazy = false,
-	config = function()
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		build = ":TSUpdate",
+		lazy = false,
+		config = function()
 		require("nvim-treesitter").setup({})
 
-		vim.api.nvim_create_autocmd("User", {
-			pattern = "LazyDone",
-			once = true,
-			callback = function()
-				require("nvim-treesitter.install").install({
-					"bash",
-					"c",
-					"cpp",
-					"go",
-					"lua",
-					"python",
-					"javascript",
-					"typescript",
-					"html",
-					"css",
-					"json",
-					"yaml",
-					"markdown",
-					"markdown_inline",
-					"vim",
-					"vimdoc",
-					"rust",
-					"java",
-					"nix",
-					"qmljs",
-				})
-			end,
-		})
-	end,
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "LazyDone",
+				once = true,
+				callback = function()
+					require("nvim-treesitter.install").install({
+						"bash",
+						"c",
+						"cpp",
+						"go",
+						"lua",
+						"python",
+						"javascript",
+						"typescript",
+						"html",
+						"css",
+						"json",
+						"yaml",
+						"markdown",
+						"markdown_inline",
+						"vim",
+						"vimdoc",
+						"rust",
+						"java",
+						"nix",
+						"qmljs",
+					})
+				end,
+			})
+		end,
+	},
+
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		event = "VeryLazy",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		opts = {
+			enable = true,
+			multiwindow = true,
+			line_numbers = false,
+			multiline_threshold = 2,
+		},
+	},
 }
