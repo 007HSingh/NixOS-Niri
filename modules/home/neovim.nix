6dd0152e6f680782826445f84d960f90f6e6c132
Harsh Singh <singhharsh25032008@gmail.com>
2026-07-31 18:52:46 +0530

    feat(neovim): move project runtimes, DAP adapters and lang formatters to devShells
    Removed from global config (will live in per-project devShells):
    - rustc, cargo (Rust devShell)
    - rustfmt, google-java-format (Rust/Java devShells)
    - python314Packages.debugpy (Python devShell)
    - vscode-extensions.vadimcn.vscode-lldb (Rust devShell)
    - vscode-extensions.vscjava.vscode-java-debug/test (Java devShell)
    - lombok (Java devShell)
    - All sessionVariables that depended on the above packages
    - Let bindings for Java debug jar discovery
    
    Kept global: all LSP servers, Nix/Lua/shell formatters+linters, tree-sitter

--- modules/home/neovim.nix
+++ modules/home/neovim.nix
@@ -8,13 +8,6 @@
 
 let
   cfg = config.modules.home.neovim;
-
-  javaDebugServerDir = "${pkgs.vscode-extensions.vscjava.vscode-java-debug}/share/vscode/extensions/vscjava.vscode-java-debug/server";
-  javaDebugJar = builtins.head (
-    builtins.filter (x: lib.hasSuffix ".jar" (baseNameOf x)) (
-      lib.filesystem.listFilesRecursive javaDebugServerDir
-    )
-  );
 in
 {
   options.modules.home.neovim.enable =
@@ -23,8 +16,6 @@ in
   config = lib.mkIf cfg.enable {
     home.packages = with pkgs; [
       neovim
-      rustc
-      cargo
 
       # LSP servers
       nixd
@@ -43,10 +34,8 @@ in
       # Formatters
       nixfmt
       stylua
-      rustfmt
       shfmt
       kdlfmt
-      google-java-format
 
       # Linters
       luajitPackages.luacheck
@@ -55,26 +44,6 @@ in
       statix
       deadnix
       tree-sitter
-
-      # DAP
-      python314Packages.debugpy
-      vscode-extensions.vadimcn.vscode-lldb
-      vscode-extensions.vscjava.vscode-java-debug
-
-      # Testing
-      vscode-extensions.vscjava.vscode-java-test
-
-      lombok
     ];
-
-    home.sessionVariables = {
-      CODELLDB_PATH = "${pkgs.vscode-extensions.vadimcn.vscode-lldb}/share/vscode/extensions/vadimcn.vscode-lldb/adapter/codelldb";
-      VSCODE_JS_DEBUG_PATH = "${pkgs.vscode-js-debug}/lib/node_modules/@vscode/js-debug/src/dapDebugServer.js";
-      JAVA_DEBUG_JAR = "${javaDebugJar}";
-      DEBUGPY_PATH = "${(pkgs.python314.withPackages (ps: [ ps.debugpy ]))}/bin/python3";
-      JAVA_TEST_JAR = "${pkgs.vscode-extensions.vscjava.vscode-java-test}/share/vscode/extensions/vscjava.vscode-java-test/server";
-      LOMBOK_JAR = "${pkgs.lombok}/share/java/lombok.jar";
-      GOOGLE_JAVA_STYLE = "${pkgs.google-java-format}";
-    };
   };
 }
